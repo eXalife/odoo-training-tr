ebe5c852309557f08f1609b32b8c9f408a5b559e
tarik.ucar <tarik.ucar@tai.com.tr>
2019-09-05 10:44:50 +0300

    Reservation

--- reservation/models/reservation.py
+++ reservation/models/reservation.py
@@ -1,6 +1,5 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
-from datetime import timedelta
 
 
 class Reservation(models.Model):
@@ -12,11 +11,11 @@ class Reservation(models.Model):
     start_date = fields.Datetime(string='Start Date', required=True) # deneme
     finish_date = fields.Datetime(string='Finish Date', required=True)
     active = fields.Boolean(string = 'Active', default=True)
-
-    @api.constrains('activity_place_id','start_date','finish_date')
-    def _check_date(self):
-        if self.entity_id and self.entity_id.is_certification_body == False:
-            raise ValidationError('It is not a certification entity')
+    #
+    # @api.constrains('activity_place_id','start_date','finish_date')
+    # def _check_date(self):
+    #     reservation = self.search([('|','&'('start_date', '<' , self.start_date),('finish_date', '>', self.finish_date)) ])
+    #         raise ValidationError('It is not a certification entity')
 
 
     # @api.model
@@ -24,3 +23,14 @@ class Reservation(models.Model):
     #     if vals.get('name', _('New')) == _('New'):
     #         vals['name'] = self.env['ir.sequence'].next_by_code('stock.landed.cost')
     #     return super(Reservation, self).create(vals)
+
+# @api.multi
+# 	def action_refuse(self):
+# 		current_employee = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
+# 		for i in self:
+#
+# 				i.write({'state': 'refuse', 'second_approver_id': current_employee.id})
+# #	Eğer şu anki state validate1 ise state'i refuse olarak değiştirilir.
+# 		return True
+
+# values['attendee_ids'] = self.env['event.registration'].search([('sale_order_line_id', '=', line.id), ('state', '!=', 'cancel')]).ids
