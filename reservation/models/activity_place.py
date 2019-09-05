12cb08463f11b4b931bef025a4cfae9f938af6f6
KerimS06 <kerimsiper@gmail.com>
2019-09-05 11:31:20 +0300

    Menuitems corrected

--- reservation/models/activity_place.py
+++ reservation/models/activity_place.py
@@ -4,6 +4,7 @@ from odoo import models, fields, api
 class ActivityPlace(models.Model):
     _name = 'activity.place'
     _description = 'Activity Place'
+    _rec_name = 'place'
 
     place = fields.Char(string='Place')
     activity_id = fields.Many2one('activity')
